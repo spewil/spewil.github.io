850f39e435122528921224528f687359e63df48d
Spencer Wilson <spencerw@mit.edu>
2017-11-12 23:03:12 +0000

    stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,61 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = true;
+        this.display = display;
+        this.laps = [];
+        this.times = [ 0, 0, 0 ];
+        this.print(this.times);
+    }
+    
+    start() {
+        if (!this.time) this.time = performance.now();
+        requestAnimationFrame(this.step.bind(this));
+    }
+    
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+    
+    calculate(timestamp) {
+
+        var diff = timestamp - this.time;
+        // Hundredths of a second are 100 ms
+        this.times[2] += diff / 10;
+        // Seconds are 100 hundredths of a second
+        if (this.times[2] >= 100) {
+            this.times[1] += 1;
+            this.times[2] -= 100;
+        }
+        // Minutes are 60 seconds
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+    
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+    
+    format(times) {
+        return `\
+${pad0(times[0], 2)}:\
+${pad0(times[1], 2)}:\
+${pad0(Math.floor(times[2]), 2)}`;
+    }   
+
+}
+
+function pad0(value, count) {
+    var result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
+let stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'));
---
+++ writings/posts/spinozasmom.md
@@ -0,0 +1,27 @@
+# spinoza's mom [draft] #  
+## materialism's 'r' us ##  
+
+meta: act like a 35-year-old (a la Jonathan Franzen)
+
+cold open 
+
+It was cold in Amsterdam, but it wasn't cold enough for three layers all day. It was the classic 2-to-3 layer jump: shirt, jumper, coat in the morning and coat-tied-around-waist for lunch. 
+
+"What do you think his mother thought?" my mother asked me, looking up at the patina'd bronze statue of Baruch. 
+"I hadn't really thought about it."
+"You wouldn't have, you're not a mother."
+"That is true."
+
+She put her coat on the back of her chair.
+
+meta: hat-hooks are the physical scenes in Amsterdam 
+	physical scenes reflect the rampant global commercialization 
+
+meta:
+	Spinoza's mom
+	Dinner (pronouns (he/she wrt gender) and (we/you wrt awards)) -- high as kites 
+	photography, homeless, biking
+	photography exhibit "i dont get it"
+	lens grinding (metaphor for subjectivity-- thats what I think)
+
+
