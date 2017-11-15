fa2b86e9540c00bd1f4f85eb6736a8e373253371
Spencer Wilson <spencerw@mit.edu>
2017-11-15 14:06:46 +0000

    tumblr API tool

--- pictures/url_grab.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-    url: "http://api.tumblr.com/v2/blog/blog.tumblr.com/info?api_key=your-api-key",
-    dataType: 'jsonp',
-    success: function(info){
-      // Print Title On the Page
-    }
-});
